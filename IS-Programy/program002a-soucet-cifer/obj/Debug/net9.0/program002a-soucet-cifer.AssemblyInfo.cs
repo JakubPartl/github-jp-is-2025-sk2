48e039656d292cdb9260d98694f195c1f250a537
Jakub Partl <jakub.partl@tul.cz>
2025-10-22 20:25:46 +0200

    #4 Součet cifer - ZBYTEK

--- IS-Programy/program002a-soucet-cifer/obj/Debug/net9.0/program002a-soucet-cifer.AssemblyInfo.cs
+++ IS-Programy/program002a-soucet-cifer/obj/Debug/net9.0/program002a-soucet-cifer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program002a-soucet-cifer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f1d12d1f81f95abedeab376d0fb32a90bc2b0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+615583af481101cb18bcebfdb8a1010d7edb7222")]
 [assembly: System.Reflection.AssemblyProductAttribute("program002a-soucet-cifer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program002a-soucet-cifer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

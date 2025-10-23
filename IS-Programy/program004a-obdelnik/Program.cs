73eb617935f5f25d3066501e2c04743eea8f8220
Jakub Partl <jakub.partl@tul.cz>
2025-10-23 15:32:07 +0200

    #7 Obdélník a - DONE

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,43 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************** Obdelník ********************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jakub Partl ******************");
+    Console.WriteLine("************** 16.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte výšku obdelníku: ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku obdelníku znovu: ");
+    }
+
+    Console.Write("Zadejte šířku obdelníku: ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku obdelníku znovu: ");
+    }
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(150));
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}

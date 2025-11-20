3e6529bd06b138e47d12f9e19ff25be5312b155f
Jakub Partl <jakub.partl@tul.cz>
2025-11-20 12:44:16 +0100

    #10 základní generátor

---
+++ IS-Programy/program007a-bubble-sort/Program.cs
@@ -0,0 +1,60 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* bubble sort ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jakub Partl ******************");
+    Console.WriteLine("************** 16.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+   Console.Write("Zadejte počet generovanách čísel (celé čísla): ");
+    int n;
+    while (!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte počet generovanách čísel znovu: ");
+    }
+
+    Console.Write("Zadejte dolní mez (celé čísla): ");
+    int lowerBound; //Dolní mez
+    while (!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu: ");
+    }
+
+    Console.Write("Zadejte horní mez (celé čísla): ");
+    int upperBound; //Horní mez
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("Zadané hodnoty: ");
+    Console.WriteLine("Počet čísel: {0}, Dolní mez: {1}, Horní mez: {2}", n, lowerBound, upperBound);
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    //deklarace pole (array)
+    int[] myRandNumbs = new int[n];
+
+    // příprava pro využití třídy Random
+    Random myRandNum = new Random();
+
+    Console.WriteLine();
+    Console.WriteLine("Náhodná čísla: ");
+    for (int i = 0; i < n; i++)
+    {
+        myRandNumbs[i] = myRandNum.Next(lowerBound, upperBound);
+        Console.Write("{0}; ", myRandNumbs[i]);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
